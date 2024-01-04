1f355be9b6d2fa30cfc5d433765dc53ce03c01f1
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2024-01-04 13:45:51 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.13.0"
+  kotlin("plugin.spring") version "1.9.22"
+}
+
+configurations {
+  testImplementation { exclude(group = "org.junit.vintage") }
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
+
+kotlin {
+  jvmToolchain(21)
+}
+
+tasks {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "21"
+    }
+  }
+}
