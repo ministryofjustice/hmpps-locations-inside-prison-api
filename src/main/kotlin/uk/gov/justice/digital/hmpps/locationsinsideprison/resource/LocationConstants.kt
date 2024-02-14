b523c2f5ac46e7b1d666ba8f6ad05b40a45b396b
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2024-02-14 13:07:26 +0000

    MAP-636 fix linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/resource/LocationConstants.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/resource/LocationConstants.kt
@@ -11,10 +11,15 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.ResponseBody
-import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.*
+import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.DeactivatedReason
+import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.LocationType
+import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.NonResidentialUsageType
+import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.ResidentialAttributeType
+import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.ResidentialAttributeValue
+import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.ResidentialHousingType
 
 @RestController
 @Validated
