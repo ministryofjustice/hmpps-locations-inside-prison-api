9bed4725cd4ca101a8bf45da705a486282f3e472
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2024-02-14 13:02:22 +0000

    MAP-636 fix linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/resource/LocationConstants.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/resource/LocationConstants.kt
@@ -13,10 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.LocationType
-import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.NonResidentialUsageType
-import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.ResidentialAttributeType
-import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.ResidentialAttributeValue
+import org.springframework.web.bind.annotation.ResponseBody
+import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.*
 
 @RestController
 @Validated
