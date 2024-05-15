5219c14e166a1aa8ac5462882a3b229d014b614e
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2024-05-15 15:44:14 +0100

    MAP-1095 add endpoint to get locations by type (#104)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/jpa/repository/LocationRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/jpa/repository/LocationRepository.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.Location
+import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.LocationType
 import java.util.UUID
 
 @Repository
@@ -10,4 +11,6 @@ interface LocationRepository : JpaRepository<Location, UUID> {
   fun findAllByPrisonIdOrderByPathHierarchy(prisonId: String): List<Location>
 
   fun findOneByPrisonIdAndPathHierarchy(prisonId: String, pathHierarchy: String): Location?
+
+  fun findAllByPrisonIdAndLocationTypeOrderByPathHierarchy(prisonId: String, locationType: LocationType): List<Location>
 }
