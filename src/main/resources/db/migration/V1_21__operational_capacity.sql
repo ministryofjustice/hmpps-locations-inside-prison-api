947dabc8098345721ba490645a3226b9b91be613
marcusaleman <marcos.aleman@digital.justice.gov.uk>
2024-05-20 15:25:45 +0100

    V19 to V21 for SQL migration file V1_19_archived_reason.sql and V1_21__operational_capacity.sql

--- src/main/resources/db/migration/V1_21__operational_capacity.sql
+++ src/main/resources/db/migration/V1_21__operational_capacity.sql
@@ -1,6 +1,7 @@
 CREATE TABLE operational_capacity
 (
-    id serial not null constraint operational_capacity_pk primary key,
+    id serial not null
+        constraint operational_capacity_pk primary key,
     capacity int not null default 0,
     prison_id varchar(3) not null unique,
     date_time timestamp not null,
