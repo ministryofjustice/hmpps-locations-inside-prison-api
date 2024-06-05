0bdb9ab74f8bd05a4514f71b34d86d2c88afc89e
Michael Willis <michael.willis@digital.justice.gov.uk>
2024-06-05 16:36:50 +0100

    MAP-1242 Update Cell class and implement data fixes in DB (#139)
    * MAP-1202 Update wheelchair accessible cells in DB
    
    The commit updates specialist_cell records in the database where the specialist_cell_type is 'WHEELCHAIR_ACCESSIBLE', setting them to 'ACCESSIBLE_CELL'. It also removes any lingering records with the 'WHEELCHAIR_ACCESSIBLE' type to ensure data consistency.
    
    * MAP-1242 Update Cell class and implement data fixes in DB
    
    Changed the update method in Cell.kt and added a new database migration file to fix DDA data. The update method now calls the super class's update method to prevent potential recursion. The database fixes include changing 'DDA_COMPLIANT' to 'ACCESSIBLE_CELL' and deleting redundant 'DDA_COMPLIANT' records.

---
+++ src/main/resources/db/migration/V1_30__data_fix_DDA.sql
@@ -0,0 +1,5 @@
+UPDATE specialist_cell ra
+set specialist_cell_type = 'ACCESSIBLE_CELL' WHERE specialist_cell_type = 'DDA_COMPLIANT'
+                                               and not exists (select 1 from specialist_cell sc where sc.location_id = ra.location_id and sc.specialist_cell_type = 'ACCESSIBLE_CELL');
+
+delete  from specialist_cell where specialist_cell_type = 'DDA_COMPLIANT';
