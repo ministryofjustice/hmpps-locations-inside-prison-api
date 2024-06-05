73811a149f70b32de34bc98f422ec14d62f184c1
Gurnank <gurnank.cheema@digital.justice.gov.uk>
2024-06-05 12:48:43 +0100

    MAP-1030 removed unused config method

--- src/main/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/config/PropertiesConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/config/PropertiesConfiguration.kt
@@ -7,13 +7,10 @@ import org.springframework.beans.factory.config.PropertiesFactoryBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.Resource
-import org.springframework.core.io.support.PropertiesLoaderUtils
-import java.io.IOException
 
 @Configuration
 open class PropertiesConfiguration(
   @Value("classpath:whereabouts/patterns/*.properties") private val resources: Array<Resource>,
-  @Value("classpath:whereabouts/enabled.properties") private val enabled: Resource,
 ) {
 
   @Bean
@@ -22,12 +19,5 @@ open class PropertiesConfiguration(
     val pfb = PropertiesFactoryBean()
     pfb.setLocations(*resources)
     return pfb
-  }
-
-  @Bean
-  @Qualifier("whereaboutsEnabled")
-  @Throws(IOException::class)
-  open fun enabled(): Set<String> {
-    return PropertiesLoaderUtils.loadProperties(enabled).stringPropertyNames()
   }
 }
