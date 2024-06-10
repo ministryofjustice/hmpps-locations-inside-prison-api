6bca76320e6edce6b5083979295ff7eee5ceff6d
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2024-06-10 14:30:19 +0100

    MAP-1244 adding PrisonLocationServiceTest

--- src/test/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/service/PrisonLocationServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/service/PrisonLocationServiceTest.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.locationsinsideprison.service
 
 
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
 import org.mockito.kotlin.any
@@ -8,6 +10,8 @@ import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.Cell
 import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.repository.CellLocationRepository
 import uk.gov.justice.digital.hmpps.locationsinsideprison.jpa.repository.LocationRepository
+import uk.gov.justice.digital.hmpps.locationsinsideprison.resource.LocationNotFoundException
+import java.util.*
 
 class PrisonLocationServiceTest {
   private val cellLocationRepository: CellLocationRepository = mock()
@@ -18,10 +22,86 @@ class PrisonLocationServiceTest {
   @Test
   fun `Get prisoners by prison id when cell location exist and location is active`() {
     val cell: Cell = mock()
-    val prisoner: Prisoner = mock()
+    val prisoner1 = Prisoner("P1", "First Name", "Last Name", "MDI", "Prison Name", "C1")
+    val prisoner2 = Prisoner("P2", "First Name", "Last Name", "MDI", "Prison Name", "C2")
+    val prisoner3 = Prisoner("P3", "First Name", "Last Name", "MDI", "Prison Name", "C2")
     whenever(cell.getPathHierarchy()).thenReturn("path")
     whenever(cellLocationRepository.findAllByPrisonIdAndActive(any(), any())).thenReturn(listOf(cell))
-    whenever(prisonerSearchService.findPrisonersInLocations(any(), any())).thenReturn(listOf(prisoner))
+    whenever(prisonerSearchService.findPrisonersInLocations(any(), any())).thenReturn(
+      listOf(
+        prisoner1,
+        prisoner2,
+        prisoner3,
+      ),
+    )
     val result = service.prisonersInPrison("MDI")
+    assertThat(result.size).isEqualTo(2)
+    assertThat(result[0].prisoners).contains(prisoner1)
+    assertThat(result[1].prisoners).contains(prisoner2, prisoner3)
   }
+
+  @Test
+  fun `Get prisoners by prison id when cell location exist and location is active and the location is empty`() {
+
+    whenever(cellLocationRepository.findAllByPrisonIdAndActive(any(), any())).thenReturn(listOf())
+    whenever(prisonerSearchService.findPrisonersInLocations(any(), any())).thenReturn(
+      listOf(),
+    )
+    val result = service.prisonersInPrison("MDI")
+    assertThat(result.size).isEqualTo(0)
+
+  }
+
+  @Test
+  fun `Get prisoners in location by key`() {
+    val cell: Cell = mock()
+    whenever(cell.getPathHierarchy()).thenReturn("path")
+    whenever(cell.cellLocations()).thenReturn(listOf(cell))
+    whenever(cell.prisonId).thenReturn("MDI")
+    val prisoner = Prisoner("P1", "First Name", "Last Name", "MDI", "Prison Name", "C1")
+
+    whenever(locationRepository.findOneByKey(any())).thenReturn(cell)
+
+    whenever(prisonerSearchService.findPrisonersInLocations(any(), any())).thenReturn(
+      listOf(prisoner),
+    )
+    val result = service.prisonersInLocations("C1")
+    assertThat(result[0].prisoners[0]).isEqualTo(prisoner)
+
+  }
+
+  @Test
+  fun `Throw LocationNotFoundException when search location by key has no results`() {
+    whenever(locationRepository.findOneByKey(any())).thenReturn(null)
+    assertThatExceptionOfType(LocationNotFoundException::class.java).isThrownBy {
+      service.prisonersInLocations("C1")
+    }
+  }
+
+  @Test
+  fun `Get prisoners in location id`() {
+    val cell: Cell = mock()
+    whenever(cell.getPathHierarchy()).thenReturn("path")
+    whenever(cell.cellLocations()).thenReturn(listOf(cell))
+    whenever(cell.prisonId).thenReturn("MDI")
+    val prisoner = Prisoner("P1", "First Name", "Last Name", "MDI", "Prison Name", "C1")
+
+    whenever(locationRepository.findById(any())).thenReturn(Optional.of(cell))
+
+    whenever(prisonerSearchService.findPrisonersInLocations(any(), any())).thenReturn(
+      listOf(prisoner),
+    )
+    val result = service.prisonersInLocations(UUID.randomUUID())
+    assertThat(result[0].prisoners[0]).isEqualTo(prisoner)
+
+  }
+
+  @Test
+  fun `Throw LocationNotFoundException when search location by id has no results`() {
+    whenever(locationRepository.findById(any())).thenReturn(Optional.empty())
+    assertThatExceptionOfType(LocationNotFoundException::class.java).isThrownBy {
+      service.prisonersInLocations(UUID.randomUUID())
+    }
+  }
+
 }
