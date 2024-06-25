253efb5fd7e6effa351ef3941e1379d69fb3bbf8
marcusaleman <marcos.aleman@digital.justice.gov.uk>
2024-06-25 14:28:15 +0100

    Used for var not required

--- src/main/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/jpa/ResidentialLocation.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/jpa/ResidentialLocation.kt
@@ -34,7 +34,6 @@ open class ResidentialLocation(
   childLocations: MutableList<Location>,
   whenCreated: LocalDateTime,
   createdBy: String,
-  usedFor: MutableSet<CellUsedFor>? = null,
 
   @Enumerated(EnumType.STRING)
   open var residentialHousingType: ResidentialHousingType = ResidentialHousingType.NORMAL_ACCOMMODATION,
