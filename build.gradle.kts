cba6f6de754e84adc26512e6a9479f4c8fdc8d9e
Michael Willis <michael.willis@digital.justice.gov.uk>
2024-06-26 10:13:34 +0100

    MAP-1355 Spring boot upgrade (correction)

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,8 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-validation")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.0.0")
-  implementation("io.opentelemetry:opentelemetry-api:1.39.0")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.5.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.0.1")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.4.0")
 
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
   implementation("com.zaxxer:HikariCP:5.1.0")
