c6e525f22ac303d9db6a3e0d108b96a227408c8c
marcusaleman <168440942+marcusaleman@users.noreply.github.com>
2024-08-01 12:03:02 +0100

    MAP-1446 uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starte… (#197)
    * MAP-1446 uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter 1.0.1 to 1.0.2 and update commons-compress to 1.26.0 which remove the vulnerability
    
    * MAP-1446 Upgrade dependencies and plugins
    
    Updated several dependencies and plugins in build.gradle.kts to their latest versions to ensure better stability and compatibility. The org.jetbrains.kotlinx.kover plugin was upgraded from 0.8.2 to 0.8.3, and various other libraries, including Springdoc, WireMock, and Testcontainers, were also updated to newer versions.
    
    ---------
    
    Co-authored-by: Michael Willis <michael.willis@digital.justice.gov.uk>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
   kotlin("plugin.spring") version "2.0.0"
   kotlin("plugin.jpa") version "2.0.0"
-  id("org.jetbrains.kotlinx.kover") version "0.8.2"
+  id("org.jetbrains.kotlinx.kover") version "0.8.3"
   idea
 }
 
@@ -16,7 +16,7 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.2-beta-3")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -28,13 +28,13 @@ dependencies {
   implementation("com.zaxxer:HikariCP:5.1.0")
   runtimeOnly("org.postgresql:postgresql")
   implementation("com.fasterxml.uuid:java-uuid-generator:5.1.0")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.2-beta")
-  testImplementation("org.wiremock:wiremock-standalone:3.7.0")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.2-beta-3")
+  testImplementation("org.wiremock:wiremock-standalone:3.9.1")
 
   testImplementation("com.pauldijou:jwt-core_2.11:5.0.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
@@ -42,8 +42,8 @@ dependencies {
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
-  testImplementation("org.testcontainers:localstack:1.19.8")
-  testImplementation("org.testcontainers:postgresql:1.19.8")
+  testImplementation("org.testcontainers:localstack:1.20.1")
+  testImplementation("org.testcontainers:postgresql:1.20.1")
 }
 
 kotlin {
