48c7c4ed88231f5ee1f6fbd3f18b27f84daa1dd0
marcusaleman <168440942+marcusaleman@users.noreply.github.com>
2024-08-19 09:44:53 +0100

    MAP-1457 42 PrisonerLocationResource map prisoner by prisonId (#229)
    * MAP-1457 PrisonerLocationResource map prisoner by prisonId
    
    * MAP-1457 PrisonerLocationResource map prisoner by code

--- src/test/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/resource/PrisonerLocationResourceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/resource/PrisonerLocationResourceTest.kt
@@ -44,14 +44,13 @@ class PrisonerLocationResourceTest : CommonDataTestBase() {
     inner class HappyPath {
 
       @Test
-      fun `can retrieve a map of prisoners in cells within this prison`() {
+      fun `can retrieve a map of prisoners in cells within this location`() {
         prisonerSearchMockServer.stubSearchByLocations(cell1.prisonId, listOf(cell1.getPathHierarchy(), cell1.getPathHierarchy()), true)
         webTestClient.get().uri("/prisoner-locations/id/${cell1.id}")
           .headers(setAuthorisation(roles = listOf("VIEW_PRISONER_LOCATIONS")))
           .exchange()
           .expectStatus().isOk
           .expectBody().json(
-            // language=json
             """
             [
               {
@@ -71,6 +70,162 @@ class PrisonerLocationResourceTest : CommonDataTestBase() {
                     "prisonId": "MDI",
                     "prisonName": "MDI",
                     "cellLocation": "Z-1-001",
+                    "firstName": "Firstname-1",
+                    "lastName": "Surname-1",
+                    "gender": "MALE"
+                }
+            ]
+          }
+          ]
+          """,
+            false,
+          )
+      }
+    }
+  }
+
+  @Nested
+  inner class PrisonerPrisonIdTest {
+    @Nested
+    inner class Security {
+
+      @Test
+      fun `access forbidden when no authority`() {
+        webTestClient.get().uri("/prisoner-locations/prison/${wingZ.prisonId}")
+          .exchange()
+          .expectStatus().isUnauthorized
+      }
+
+      @Test
+      fun `access forbidden when no role`() {
+        webTestClient.get().uri("/prisoner-locations/prison/${wingZ.prisonId}")
+          .headers(setAuthorisation(roles = listOf()))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+
+      @Test
+      fun `access forbidden with wrong role`() {
+        webTestClient.get().uri("/prisoner-locations/prison/${wingZ.prisonId}")
+          .headers(setAuthorisation(roles = listOf("ROLE_BANANAS")))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+    }
+
+    @Nested
+    inner class HappyPath {
+
+      @Test
+      fun `can retrieve a map of prisoners in cells within this prisonId`() {
+        prisonerSearchMockServer.stubSearchByLocations(
+          cell1N.prisonId,
+          listOf(cell1N.getPathHierarchy(), cell1N.getPathHierarchy()),
+          true,
+        )
+
+        webTestClient.get().uri("/prisoner-locations/prison/${cell1N.prisonId}")
+          .headers(setAuthorisation(roles = listOf("VIEW_PRISONER_LOCATIONS")))
+          .exchange()
+          .expectStatus().isOk
+          .expectBody().json(
+            """
+            [
+              {
+                "cellLocation": "A-1-001",
+                "prisoners": [
+                  {
+                    "prisonerNumber": "A0000AA",
+                    "prisonId": "NMI",
+                    "prisonName": "NMI",
+                    "cellLocation": "A-1-001",
+                    "firstName": "Firstname-0",
+                    "lastName": "Surname-0",
+                    "gender": "MALE"
+                  },
+                  {
+                    "prisonerNumber": "A0001AA",
+                    "prisonId": "NMI",
+                    "prisonName": "NMI",
+                    "cellLocation": "A-1-001",
+                    "firstName": "Firstname-1",
+                    "lastName": "Surname-1",
+                    "gender": "MALE"
+                }
+            ]
+          }
+          ]
+          """,
+            false,
+          )
+      }
+    }
+  }
+
+  @Nested
+  inner class PrisonerLocationKeyTest {
+    @Nested
+    inner class Security {
+
+      @Test
+      fun `access forbidden when no authority`() {
+        webTestClient.get().uri("/prisoner-locations/key/${wingZ.getKey()}")
+          .exchange()
+          .expectStatus().isUnauthorized
+      }
+
+      @Test
+      fun `access forbidden when no role`() {
+        webTestClient.get().uri("/prisoner-locations/prison/${wingZ.getKey()}")
+          .headers(setAuthorisation(roles = listOf()))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+
+      @Test
+      fun `access forbidden with wrong role`() {
+        webTestClient.get().uri("/prisoner-locations/prison/${wingZ.getKey()}")
+          .headers(setAuthorisation(roles = listOf("ROLE_BANANAS")))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+    }
+
+    @Nested
+    inner class HappyPath {
+
+      @Test
+      fun `can retrieve a map of prisoners in cells within this code`() {
+        prisonerSearchMockServer.stubSearchByLocations(
+          cell1N.prisonId,
+          listOf(cell1N.getPathHierarchy(), cell1N.getPathHierarchy()),
+          true,
+        )
+
+        webTestClient.get().uri("/prisoner-locations/key/${cell1N.getKey()}")
+          .headers(setAuthorisation(roles = listOf("VIEW_PRISONER_LOCATIONS")))
+          .exchange()
+          .expectStatus().isOk
+          .expectBody().json(
+            """
+            [
+              {
+                "cellLocation": "A-1-001",
+                "prisoners": [
+                  {
+                    "prisonerNumber": "A0000AA",
+                    "prisonId": "NMI",
+                    "prisonName": "NMI",
+                    "cellLocation": "A-1-001",
+                    "firstName": "Firstname-0",
+                    "lastName": "Surname-0",
+                    "gender": "MALE"
+                  },
+                  {
+                    "prisonerNumber": "A0001AA",
+                    "prisonId": "NMI",
+                    "prisonName": "NMI",
+                    "cellLocation": "A-1-001",
                     "firstName": "Firstname-1",
                     "lastName": "Surname-1",
                     "gender": "MALE"
