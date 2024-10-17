c91abd44700663e4f1da8f99427d957d851e68c5
Michael Willis <michael.willis@digital.justice.gov.uk>
2024-10-17 16:12:22 +0100

    MAP-1678 Reset training once a week on Monday morning

--- src/test/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/resource/ResetTrainingEnvironmentResourceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/resource/ResetTrainingEnvironmentResourceTest.kt
@@ -3,10 +3,8 @@ package uk.gov.justice.digital.hmpps.locationsinsideprison.resource
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.springframework.test.context.ActiveProfiles
 import uk.gov.justice.digital.hmpps.locationsinsideprison.integration.SqsIntegrationTestBase
 
-@ActiveProfiles("test")
 class ResetTrainingEnvironmentResourceTest : SqsIntegrationTestBase() {
 
   @DisplayName("PUT /reset-training")
