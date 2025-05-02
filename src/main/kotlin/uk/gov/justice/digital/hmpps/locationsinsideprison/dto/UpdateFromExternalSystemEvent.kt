b1bb5c7517e24d1d8b34285eef0ade67dd79d1c8
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-02 12:13:16 +0100

    New models for queue events and specific deactivate request

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/dto/UpdateFromExternalSystemEvent.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.locationsinsideprison.dto
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+
+data class UpdateFromExternalSystemEvent(
+  val messageId: String,
+  val eventType: String,
+  val description: String? = null,
+  val messageAttributes: Map<String, Any?> = emptyMap(),
+  val who: String? = null,
+) {
+  fun toTemplarilyDeactivateLocationByKeyRequest(): TemplarilyDeactivateLocationByKeyRequest {
+    val mapper = ObjectMapper().registerModule(KotlinModule.Builder().build()).registerModule(JavaTimeModule())
+    return mapper.convertValue(this.messageAttributes, TemplarilyDeactivateLocationByKeyRequest::class.java)
+  }
+}
