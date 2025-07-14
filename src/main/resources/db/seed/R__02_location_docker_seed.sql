c7bbfb3cae491dffc2c28803a9c5e9a858daf934
Michael Willis <Michael.Willis2@justice.gov.uk>
2025-07-14 10:16:34 +0100

    MAP-2571 Update prison configuration certification logic in seed and routine SQL
    - Set `certification_approval_required` to `true` in `R__02_location_docker_seed.sql`.
    - Adjust `certification_approval_required` default value to `false` in `R__01_location_procedures.sql`.

--- src/main/resources/db/seed/R__02_location_docker_seed.sql
+++ src/main/resources/db/seed/R__02_location_docker_seed.sql
@@ -1,3 +1,5 @@
 call setup_prison_demo_locations('LEI', 'ITAG_USER');
 call setup_prison_demo_locations('BXI', 'ITAG_USER');
 call setup_prison_demo_locations('MDI', 'ITAG_USER');
+
+update prison_configuration set certification_approval_required = true;
