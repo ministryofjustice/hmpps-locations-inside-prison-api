19351cce23482f1399f07d267cb7ed6821976e52
Michael Willis <Michael.Willis2@justice.gov.uk>
2025-11-25 11:57:48 +0000

    Update TestContainers dependencies and refactor container configurations

--- src/test/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/config/PostgresContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/locationsinsideprison/config/PostgresContainer.kt
@@ -1,22 +1,21 @@
 package uk.gov.justice.digital.hmpps.locationsinsideprison.config
 
 import org.slf4j.LoggerFactory
-import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.postgresql.PostgreSQLContainer
 import java.io.IOException
 import java.net.ServerSocket
 
 object PostgresContainer {
-  val instance: PostgreSQLContainer<Nothing>? by lazy { startPostgresqlContainer() }
+  val instance: PostgreSQLContainer? by lazy { startPostgresqlContainer() }
 
-  private fun startPostgresqlContainer(): PostgreSQLContainer<Nothing>? {
-    TestContainersUtil.setDockerApiVersion()
+  private fun startPostgresqlContainer(): PostgreSQLContainer? {
     if (isPostgresRunning()) {
       log.warn("Using existing Postgres database")
       return null
     }
     log.info("Creating a Postgres database")
-    return PostgreSQLContainer<Nothing>("postgres").apply {
+    return PostgreSQLContainer("postgres").apply {
       withEnv("HOSTNAME_EXTERNAL", "localhost")
       withDatabaseName("locations_inside_prison")
       withUsername("locations_inside_prison")
