57d2b63c6cde05c0296794906dbbfedb936a2d3a
Michael Willis <michael.willis2@justice.gov.uk>
2026-03-30 16:24:38 +0100

    Chore - dep updates (#607)
    - `uk.gov.justice.hmpps.gradle-spring-boot`: `10.0.5` -> `10.1.2`
    - `org.jetbrains.kotlin.plugin.spring`: `2.3.10` -> `2.3.20`
    - `org.jetbrains.kotlin.plugin.jpa`: `2.3.10` -> `2.3.20`
    - `org.jetbrains.kotlinx.kover`: `0.9.7` -> `0.9.8`
    - `uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter`: `2.0.2` -> `2.1.0`
    - `uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter`: `7.0.1` -> `7.3.0`
    - `io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations`: `2.25.0` -> `2.26.1`
    - `uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test`: `2.0.0` -> `2.1.0`
    - `io.swagger.parser.v3:swagger-parser`: `2.1.37` -> `2.1.39`
    - `org.testcontainers:testcontainers-localstack`: `2.0.3` -> `2.0.4`
    - `org.testcontainers:testcontainers-postgresql`: `2.0.3` -> `2.0.4`
    
    ## Related test change
    
    - Updated the OpenAPI UI test URL from `/swagger-ui/index.html?configUrl=/v3/api-docs` to `/webjars/swagger-ui/index.html?configUrl=/v3/api-docs`

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ import uk.gov.justice.digital.hmpps.gradle.PortForwardRedisTask
 import uk.gov.justice.digital.hmpps.gradle.RevealSecretsTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.5"
-  kotlin("plugin.spring") version "2.3.10"
-  kotlin("plugin.jpa") version "2.3.10"
-  id("org.jetbrains.kotlinx.kover") version "0.9.7"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.1.2"
+  kotlin("plugin.spring") version "2.3.20"
+  kotlin("plugin.jpa") version "2.3.20"
+  id("org.jetbrains.kotlinx.kover") version "0.9.8"
   idea
 }
 
@@ -16,8 +16,8 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.2")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.0.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.1.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.3.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-flyway")
@@ -26,7 +26,7 @@ dependencies {
   implementation("org.springframework.security:spring-security-access")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("uk.gov.justice.service.hmpps:hmpps-digital-prison-reporting-lib:9.12.2")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.25.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.26.1")
   implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.60.1")
 
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
@@ -35,7 +35,7 @@ dependencies {
   implementation("com.fasterxml.uuid:java-uuid-generator:5.2.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.0.0")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.1.0")
   testImplementation("org.springframework.boot:spring-boot-starter-webclient-test")
   testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
   testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
@@ -44,13 +44,13 @@ dependencies {
   testImplementation("com.pauldijou:jwt-core_2.11:5.0.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.37") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.39") {
     exclude(group = "io.swagger.core.v3")
   }
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
-  testImplementation("org.testcontainers:testcontainers-localstack:2.0.3")
-  testImplementation("org.testcontainers:testcontainers-postgresql:2.0.3")
+  testImplementation("org.testcontainers:testcontainers-localstack:2.0.4")
+  testImplementation("org.testcontainers:testcontainers-postgresql:2.0.4")
 }
 
 kotlin {
